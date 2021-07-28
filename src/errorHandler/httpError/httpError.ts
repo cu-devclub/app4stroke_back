bdbd3111a04494f5b8aca4118e74473317d8f283
DefeNerTH <sern.dev@gmail.com>
2021-07-28 10:09:13 +0700

    compost in httpError

--- src/errorHandler/httpError/httpError.ts
+++ src/errorHandler/httpError/httpError.ts
@@ -1,4 +1,19 @@
-import Api404 from "./Component/NotFoundError";
+import BadRequestError from "./Component/BadRequestError";
+import ConflictError from "./Component/conflictError";
+import ForbiddenError from "./Component/ForbiddenError";
+import InternalServerError from "./Component/InternalServerError";
+import NotFoundError from "./Component/NotFoundError";
+import RequestTimeoutError from "./Component/RequestTimeoutError";
+import TooManyRequestError from "./Component/TooManyRequestError";
+import UnauthorizedError from "./Component/UnauthorizedError";
 
-
-export { Api404 }
+export {
+  BadRequestError,
+  ConflictError,
+  ForbiddenError,
+  InternalServerError,
+  NotFoundError,
+  RequestTimeoutError,
+  TooManyRequestError,
+  UnauthorizedError
+};
