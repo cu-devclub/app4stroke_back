9b6c79ea0c76e326252994d78ad7ff75fd115e2e
tem13 <temt4510@gmail.com>
2021-08-10 19:58:30 +0700

    update storage functions

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,12 +1,7 @@
 import util from "util";
 import multer from "multer";
 
-const maxSize = 2 * 1024 * 1024;
-
-let processFile = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: maxSize },
-}).single("file");
+let processFile = multer().single("file");
 
 let processFileMiddleware = util.promisify(processFile);
 
