4b7c84ed3cba4c7d93ba41f9ac4cc1d52f7a0411
DefeNerTH <sern.dev@gmail.com>
2021-08-16 17:59:48 +0700

    add api route analyseDicom

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import healthRouter from './health';
 import predictScoreRouter from './predictScore';
 import fileRouter from './file';
+import analyseDicom from './analyseDicom';
 
 const router = Router();
 
 router.use(healthRouter);
 router.use(predictScoreRouter);
+router.use(analyseDicom);
 // router.use(authRouter);
 router.use('/files', fileRouter);
 
