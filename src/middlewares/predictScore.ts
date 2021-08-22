267a925a19721b91664e546ad6501556d6e8f7e7
DefeNerTH <sern.dev@gmail.com>
2021-08-22 19:05:02 +0700

    snapshot

--- src/middlewares/predictScore.ts
+++ src/middlewares/predictScore.ts
@@ -2,10 +2,11 @@ import Joi from 'joi';
 import { Request, Response } from 'express';
 import axios, { AxiosRequestConfig } from 'axios';
 import httpError from '../errorHandler/httpError/httpError';
+import analyseDicom from './analyzeDicom';
 
 const dataFromFontSchema = Joi.object({
   PatientInformation: Joi.object({
-    patientID: Joi.any().required(),
+    patientID: Joi.number().required(),
     age: Joi.number().required(),
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
@@ -38,7 +39,7 @@ const dataFromFontSchema = Joi.object({
     vertigo: Joi.boolean().required(),
     visualProblem: Joi.boolean().required(),
     other: Joi.boolean().required(),
-    otherText: Joi.string(),
+    otherText: Joi.string().required(),
   }).required(),
   Underlying: Joi.object({
     deny: Joi.boolean().required(),
@@ -56,7 +57,7 @@ const dataFromFontSchema = Joi.object({
     obesity: Joi.boolean().required(),
     smoking: Joi.boolean().required(),
     other: Joi.boolean().required(),
-    otherText: Joi.string(),
+    otherText: Joi.string().required(),
   }).required(),
   VitalSigns: Joi.object({
     systolicBP: Joi.number().required(),
@@ -84,72 +85,6 @@ const dataFromFontSchema = Joi.object({
   }).required(),
 });
 
-const mapFrontToMl = async (body: any) => {
-  return {
-    age: body.PatientInformation.age,
-    arrival_date: body.PatientInformation.arrivalDate,
-    arrival_time: body.PatientInformation.arrivalTime,
-    onset: body.PatientInformation.onset,
-    clear_onset_date: body.PatientInformation.clearDate,
-    clear_onset_time: body.PatientInformation.clearTime,
-    lsn_date: body.PatientInformation.lastDate,
-    lsn_time: body.PatientInformation.lastTime,
-    fsa_date: body.PatientInformation.firstDate,
-    fsa_time: body.PatientInformation.firstTime,
-    time_course: body.ChiefComplaint.timeCourse,
-    cc_alter_cons: body.ChiefComplaint.alterationOfConsciousness,
-    cc_facial_weak: body.ChiefComplaint.facialWeakness,
-    cc_facial_weak_left: body.ChiefComplaint.facialWeaknessLeft,
-    cc_facial_weak_right: body.ChiefComplaint.facialWeaknessRight,
-    cc_hemiparesis: body.ChiefComplaint.hemiparesis,
-    cc_hemiparesis_left: body.ChiefComplaint.hemiparesisLeft,
-    cc_hemiparesis_right: body.ChiefComplaint.hemiparesisRight,
-    cc_hemiparesthesia: body.ChiefComplaint.hemiparesthesia,
-    cc_hemiparesthesia_left: body.ChiefComplaint.hemiparesthesiaLeft,
-    cc_hemiparesthesia_right: body.ChiefComplaint.hemiparesthesiaRight,
-    cc_dysarthria: body.ChiefComplaint.dysarthria,
-    cc_aphasia: body.ChiefComplaint.aphasia,
-    cc_ataxia: body.ChiefComplaint.ataxia,
-    cc_vertigo: body.ChiefComplaint.vertigo,
-    cc_visual: body.ChiefComplaint.visualProblem,
-    cc_others: body.ChiefComplaint.other,
-    cc_others_text: body.ChiefComplaint.otherText,
-    ud_deny_ud: body.Underlying.deny,
-    ud_hx_tia: body.Underlying.hx,
-    ud_prev_tia: body.Underlying.previousTia,
-    ud_prev_stroke: body.Underlying.previousStroke,
-    ud_ht: body.Underlying.ht,
-    ud_dm: body.Underlying.dm,
-    ud_dlp: body.Underlying.dlp,
-    ud_valvular_hd: body.Underlying.valvularHeartDisease,
-    ud_af: body.Underlying.af,
-    ud_coronary_hd: body.Underlying.coronaryHeartDisease,
-    ud_ckd: body.Underlying.ckd,
-    ud_peripheral_ad: body.Underlying.peripheralArterialDisease,
-    ud_obesity: body.Underlying.obesity,
-    ud_smoking: body.Underlying.smoking,
-    ud_others: body.Underlying.other,
-    ud_others_text: body.Underlying.otherText,
-    systolic_bp: body.VitalSigns.systolicBP,
-    diastolic_bp: body.VitalSigns.diastolicBP,
-    nihss_1a: body.NIHSS.levelOfConsciousness,
-    nihss_1b: body.NIHSS.twoQuestions,
-    nihss_1c: body.NIHSS.twoCommands,
-    nihss_2: body.NIHSS.bestGaze,
-    nihss_3: body.NIHSS.bestVisual,
-    nihss_4: body.NIHSS.facialPalsy,
-    nihss_5a: body.NIHSS.bestMotorLeftArm,
-    nihss_5b: body.NIHSS.bestMotorRightArm,
-    nihss_6a: body.NIHSS.bestMotorLeftLeg,
-    nihss_6b: body.NIHSS.bestMotorRightLeg,
-    nihss_7: body.NIHSS.limbAtaxia,
-    nihss_8: body.NIHSS.sensory,
-    nihss_9: body.NIHSS.bestLanguageAphasia,
-    nihss_10: body.NIHSS.dysarthria,
-    nihss_11: body.NIHSS.extinctionOrNeglect,
-  };
-};
-
 const config: AxiosRequestConfig = {
   method: 'post',
   url: '/',
@@ -160,12 +95,12 @@ const config: AxiosRequestConfig = {
 
 const predictScore = async (req: Request, res: Response) => {
   try {
-    const analyse = 
+    // const analyse = analyseDicom()
     const data = await dataFromFontSchema.validateAsync(req.body);
-    const dataMlForm = await mapFrontToMl(data);
-    const post = await axios({ ...config, data: dataMlForm });
+    // const dataMlForm = await mapFrontToMl(data);
+    // const post = await axios({ ...config, data: dataMlForm });
 
-    res.json(post.data);
+    // res.json(post.data);
   } catch (e: any) {
     console.log(e);
     if (e.code === 'ECONNREFUSED') {
