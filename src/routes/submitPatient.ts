7799df8b7a0df9b447e6dfef928c86f00e6485c0
DefeNerTH <sern.dev@gmail.com>
2021-08-22 19:04:50 +0700

    multer multi upload

--- src/routes/submitPatient.ts
+++ src/routes/submitPatient.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import submitPatient from '../controllers/submitPatient';
+import multer from 'multer';
+const upload = multer({ dest: 'temp/' });
+
 const router = Router();
 
-router.post('/submitPatient', submitPatient);
+router.post('/submitPatient', upload.array('file'), submitPatient);
 
 export default router;
