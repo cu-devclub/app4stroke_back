66c18be550543cdd1bf9b07df5bd7eaa17c6f3df
DefeNerTH <sern.dev@gmail.com>
2021-08-29 16:05:18 +0700

    update

--- src/models/predictData.ts
+++ src/models/predictData.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 export interface predict {
-  totalSlices: String;
-  maxScoreSlice: String;
-  maxCtScore: String;
+  totalSlices: Number;
+  maxScoreSlice: Number;
+  maxCtScore: Number;
   imgPath: Array<string>;
   heatmapPath: Array<string>;
   ctScores: Array<number>;
@@ -20,16 +20,16 @@ const predictSchema = new mongoose.Schema({
   totalSlices: { type: String, default: '' },
   maxScoreSlice: { type: String, default: '' },
   maxCtScore: { type: String, default: '' },
-  imgPath: { type: Array, of: String, default: '' },
-  heatmapPath: { type: Array, of: String, default: '' },
-  ctScores: { type: Array, of: String, default: '' },
-  prop: { type: String, require: true },
-  topPosFactors: { type: Array, of: String, default: '' },
-  topPosValues: { type: Array, of: String, default: '' },
-  topPosImpacts: { type: Array, of: String, default: '' },
-  topNegFactors: { type: Array, of: String, default: '' },
-  topNegValues: { type: Array, of: String, default: '' },
-  topNegImpacts: { type: Array, of: String, default: '' },
+  imgPath: { type: Array, of: String, default: [] },
+  heatmapPath: { type: Array, of: String, default: [] },
+  ctScores: { type: Array, of: String, default: [] },
+  prop: { type: String, default: '' },
+  topPosFactors: { type: Array, of: String, default: [] },
+  topPosValues: { type: Array, of: String, default: [] },
+  topPosImpacts: { type: Array, of: String, default: [] },
+  topNegFactors: { type: Array, of: String, default: [] },
+  topNegValues: { type: Array, of: String, default: [] },
+  topNegImpacts: { type: Array, of: String, default: [] },
   addDate: { type: Date, default: Date.now() },
   testID: { type: Number, required: true },
 });
