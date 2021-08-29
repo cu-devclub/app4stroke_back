a60aab00aa619c18af3046e78bdf5b8d2a15b217
DefeNerTH <sern.dev@gmail.com>
2021-08-29 17:08:35 +0700

    update: done view

--- src/controllers/view.ts
+++ src/controllers/view.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from 'express';
+import Joi from 'joi';
+import BaseError from '../errorHandler/httpError/Component/baseError';
+import httpError from '../errorHandler/httpError/httpError';
+import { findInfo } from '../middlewares/patient';
+import { findPredict } from '../middlewares/predict';
 
-const view = async (req: Request, res: Response) => {};
+const schema = Joi.object({
+  id: Joi.number().required(),
+});
+
+const view = async (req: Request, res: Response) => {
+  try {
+    const data = await schema.validateAsync(req.body);
+
+    const info = await findInfo(data.id);
+    const predict = await findPredict(data.id);
+    res.status(200).send({
+      data: {
+        information: info,
+        prediction: predict,
+      },
+    });
+  } catch (e: any) {
+    if (e instanceof BaseError) {
+      res.status(e.statusCode).send(e);
+    } else if (e instanceof Joi.ValidationError) {
+      res.status(400).send(httpError(400, `${e.name}:${e.message}`));
+    }
+  }
+};
 
 export default view;
