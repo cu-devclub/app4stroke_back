dce49eaee109a138bf32d04fc6c247ed8d56de91
tem13 <temt4510@gmail.com>
2021-09-01 18:59:01 -0400

    return S3 URI on success

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -4,17 +4,19 @@ import path from 'path';
 
 import s3 from '../config/s3';
 
+let fullPath = "";
+
 const upload = (foldername: string, filename: string) => {
     return multer({
         storage: multerS3({
             s3: s3,
             bucket: 'c4ab726d-2d35-448c-8352-93f18fdbcd62',
             key: function (req: any, file: any, cb: any) {
-                const fullPath = path.join(foldername, filename);
+                fullPath = path.join(foldername, filename + path.extname(file.originalname));
                 cb(null, fullPath);
             }
-        })
+        }),
     });
 }
 
-export default upload;
+export { upload, fullPath };
--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import fileController from '../controllers/file';
-import upload from '../middlewares/upload';
+import { upload } from '../middlewares/upload';
 
 const router = Router();
 
-router.post('/upload', upload("foldername", "filename").single("file"), fileController.upload);
+router.post('/upload', upload("foldername1", "filename").single("file"), fileController.upload);
 router.get('/', fileController.getFileNames);
 router.get('/:foldername/:filename', fileController.download);
  
