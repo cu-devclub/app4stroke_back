c95a0fea1fbe646727617abc2fecb63b32b09995
DefeNerTH <sern.dev@gmail.com>
2021-09-05 23:45:12 +0700

    pretter auto format

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,19 +3,25 @@ import path from 'path';
 import FileType from 'file-type';
 
 const upload = async (buffer: any, filePath: string, fileName: string) => {
-    const { ext, mime } = await FileType.fromBuffer(buffer) || { ext: 'unknown', mime: 'unknown' }
-    console.log(mime);
+  const { ext, mime } = (await FileType.fromBuffer(buffer)) || {
+    ext: 'unknown',
+    mime: 'unknown',
+  };
+  console.log(mime);
 
-    const fullPath = path.join(filePath, fileName+"."+ext)
-    const file = bucket.file(fullPath);
+  const fullPath = path.join(filePath, fileName + '.' + ext);
+  const file = bucket.file(fullPath);
 
-    file.save(buffer, (err: any) => {
-        if (err) {
-            console.log(err);
-        }
-    });
+  file.save(buffer, (err: any) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 
-    return { url: `https://storage.cloud.google.com/stroke_images_3/${fullPath}`, gsutilURI: `gs://stroke_images_3/${fullPath}` };
-}
+  return {
+    url: `https://storage.cloud.google.com/stroke_images_3/${fullPath}`,
+    gsutilURI: `gs://stroke_images_3/${fullPath}`,
+  };
+};
 
 export default upload;
