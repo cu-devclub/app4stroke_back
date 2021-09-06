9488dfc3aecc861eacab9a15fe97c6e4cedfa431
DefeNerTH <sern.dev@gmail.com>
2021-09-06 13:13:31 +0700

    new: api 'result'

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,12 +4,14 @@ import userRouter from './user';
 import fileRouter from './file';
 import submitPatientRouter from './submitPatient';
 import view from './view';
+import result from './result';
 
 const router = Router();
 
 router.use(healthRouter);
 router.use(submitPatientRouter);
 router.use(view);
+router.use(result);
 // router.use(authRouter);
 router.use('/user', userRouter);
 router.use('/files', fileRouter);
