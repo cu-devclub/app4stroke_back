82bea11ff1e4dd7f1f8f6b1c6facadccafa2a5eb
yokeTH <sern.dev@gmail.com>
2021-10-08 00:56:51 +0700

    change to S3

--- src/config/storage.ts
+++ src/config/storage.ts
@@ -1,2 +1,11 @@
-import { googleStorage } from './cloudStorage';
-export default new googleStorage('google-cloud-key.json', 'stroke_images_3');
+import { S3storage } from './cloudStorage';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default new S3storage(
+  process.env.AWS_ACCESS_KEY_ID || '',
+  process.env.AWS_SECRET_ACCESS_KEY || '',
+  process.env.AWS_DEFAULT_REGION || '',
+  process.env.AWS_BUCKET_NAME || '',
+);
