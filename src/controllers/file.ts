d53eaba6398910da8dee07ce1a5a0ae858b7c78e
yokeTH <sern.dev@gmail.com>
2021-10-08 00:57:07 +0700

    prototype Download

--- src/controllers/file.ts
+++ src/controllers/file.ts
@@ -3,6 +3,7 @@ import httpError from '../errorHandler/httpError/httpError';
 
 import upload from '../middlewares/upload';
 import download from '../middlewares/download';
+import storage from '../config/storage';
 
 export default {
   upload: async (req: Request, res: Response) => {
@@ -17,14 +18,25 @@ export default {
     }
   },
   download: async (req: Request, res: Response) => {
-    try {
-      const filePath = '';
+    // try {
+    //   const filePath = '';
 
-      await download(filePath, req, res);
-    } catch (err) {
-      res
-        .status(500)
-        .send(httpError(500, 'Could not download the file. ' + err));
-    }
+    //   await download(filePath, req, res);
+    // } catch (err) {
+    //   res
+    //     .status(500)
+    //     .send(httpError(500, 'Could not download the file. ' + err));
+    // }
+    try {
+      const dl = await storage.download(req.params.path);
+      if (dl instanceof Error) {
+        throw new Error();
+      }
+      if (!(typeof dl.ContentType === 'string')) {
+        throw new Error();
+      }
+      res.setHeader('Content-Type', dl.ContentType);
+      res.send(dl.Body);
+    } catch (error: any) {}
   },
 };
