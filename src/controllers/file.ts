020bf9b86d7364c5f9ba8d99a8f62a4c879658bc
yokeTH <sern.dev@gmail.com>
2021-12-06 14:55:40 +0700

    remove unnessery function

--- src/controllers/file.ts
+++ src/controllers/file.ts
@@ -6,27 +6,7 @@ import download from '../middlewares/download';
 import storage from '../config/storage';
 
 export default {
-  upload: async (req: Request, res: Response) => {
-    try {
-      const buffer = '';
-
-      const URI = await upload(buffer, 'ml', 'filename');
-
-      res.status(200).send(URI);
-    } catch (err) {
-      res.status(500).send(httpError(500, 'Could not upload the file. ' + err));
-    }
-  },
   download: async (req: Request, res: Response) => {
-    // try {
-    //   const filePath = '';
-
-    //   await download(filePath, req, res);
-    // } catch (err) {
-    //   res
-    //     .status(500)
-    //     .send(httpError(500, 'Could not download the file. ' + err));
-    // }
     try {
       const dl = await storage.download(req.params.path);
       if (dl instanceof Error) {
