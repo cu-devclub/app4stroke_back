815af4287c6894849c090a1a6711932fd17c42f1
yokeTH <sern.dev@gmail.com>
2021-12-06 14:46:40 +0700

    rm /me; change /login controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import userController from '../controllers/user';
-import auth from '../middlewares/auth';
+import auth from '../middlewares/decodeToken';
+import login from '../controllers/login';
 
 const router = Router();
 
 router.post('/signup', userController.signup);
-router.post('/login', userController.login);
-router.get('/me', auth, userController.me);
+router.post('/login', login);
 
 router.get('/', userController.getUser);
 router.post('/', userController.postUser);
