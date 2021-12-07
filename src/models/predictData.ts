7bbff792fa0c85068bcc46e8aadc5c468602bfba
yokeTH <sern.dev@gmail.com>
2021-12-08 00:56:10 +0700

    refactor code more

--- src/models/predictData.ts
+++ src/models/predictData.ts
@@ -4,9 +4,9 @@ export interface record {
   total_slices: Number;
   max_score_slice: Number;
   max_ct_score: Number;
-  imgPath: Array<string>;
+  img_path: Array<string>;
   heatmap_path: Array<string>;
-  ctScores: Array<number>;
+  ct_score: Array<number>;
   prob: Number;
   top_pos_factors: Array<string>;
   top_pos_values: Array<string>;
@@ -21,21 +21,21 @@ export interface recordInDb extends record {
 }
 
 const recordSchema = new mongoose.Schema({
-  total_slices: { type: String, default: '' },
-  max_score_slice: { type: String, default: '' },
-  max_ct_score: { type: String, default: '' },
-  img_path: { type: Array, of: String, default: [] },
-  heatmap_path: { type: Array, of: String, default: [] },
-  ct_score: { type: Array, of: String, default: [] },
-  prob: { type: String, default: '' },
-  top_pos_factors: { type: Array, of: String, default: [] },
-  top_pos_values: { type: Array, of: String, default: [] },
-  top_pos_impacts: { type: Array, of: String, default: [] },
-  top_neg_factors: { type: Array, of: String, default: [] },
-  top_neg_values: { type: Array, of: String, default: [] },
-  top_neg_impacts: { type: Array, of: String, default: [] },
-  add_date: { type: Date, default: Date.now() },
   recordID: { type: Number, required: true },
+  total_slices: { type: Number, required: true },
+  max_score_slice: { type: Number, required: true },
+  max_ct_score: { type: Number, required: true },
+  img_path: { type: Array, of: String, required: true },
+  heatmap_path: { type: Array, of: String, required: true },
+  ct_score: { type: Array, of: Number, required: true },
+  prob: { type: Number, required: true },
+  top_pos_factors: { type: Array, of: String, required: true },
+  top_pos_values: { type: Array, of: String, required: true },
+  top_pos_impacts: { type: Array, of: Number, required: true },
+  top_neg_factors: { type: Array, of: String, required: true },
+  top_neg_values: { type: Array, of: String, required: true },
+  top_neg_impacts: { type: Array, of: Number, required: true },
+  create_date: { type: Date, default: Date.now() },
 });
 
 export default mongoose.model('record', recordSchema);
